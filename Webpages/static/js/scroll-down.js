5845e0936f23465d484d272818490d067198ee36
imcoolthanks <wongq9999@outlook.com>
2022-01-27 13:43:42 +0000

    Use base.css

--- Webpages/static/js/scroll-down.js
+++ Webpages/static/js/scroll-down.js
@@ -2,8 +2,30 @@ $(function() {
     $('section a, .nav-link-heading').on('click', function(e) {
       e.preventDefault();
       
-      const href = $(this).attr("href");
-      $("html, body").animate({ scrollTop: $(href).offset().top }, 500);
+      //get where you want to go
+      let href = $(this).attr("href");
+      
+      //get current file
+      let pathname = $(location).attr('pathname');
+
+      //check if its contact as it exists on both pages
+      if (href == "#section-Contact")
+      {
+        $("html, body").animate({ scrollTop: $(href).offset().top }, 500);
+      }
+      
+      //check if on different page
+      //from portfolio to home page
+      else if (pathname=="/portfolio" && (!href.startsWith("/portfolio")))
+      {
+        location.replace("/" + href);
+      }
+
+      //is on same page
+      else
+      {
+        $("html, body").animate({ scrollTop: $(href).offset().top }, 500);
+      }
     });
 });
 
